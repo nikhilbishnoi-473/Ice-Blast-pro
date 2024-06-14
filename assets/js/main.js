5b69d6cfaf85498407ae4519f6e05def9b24b0a1
Nikhil Bishnoi <nikhilbishnoi@Nikhils-MacBook-Air.local>
2024-06-14 10:39:41 +0530

    slider start

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,44 @@ function openNav_1() {
 }
 
 
-
+$('.responsive').slick({
+  dots: false,
+  centerMode: true,
+  centerPadding: '30px',
+  adaptiveHeight: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 $('.responsive_2').slick({
   dots: false,
   arrows: false,
